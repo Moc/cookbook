0968c2275e2d3ca2a7cb55a4d95e1b49aebffbcf
Moc <github@tijnkuyper.nl>
2021-04-11 19:20:47 +0200

    Add SEF alias

--- e_url.php
+++ e_url.php
@@ -17,42 +17,49 @@ class cookbook_url
 		$config = array();
 
 		$config['index'] = array(
+			'alias'         => 'cookbook',
 			'regex'			=> '^recepten/?$',
 			'sef'			=> 'recepten',
 			'redirect'		=> '{e_PLUGIN}cookbook/index.php',
 		);
 
 		$config['id'] = array(
+			'alias'         => 'cookbook',
 			'regex'			=> '^recepten/id/(.*)$',
 			'sef'			=> 'recepten/id/{id}/{name}',
 			'redirect'		=> '{e_PLUGIN}cookbook/index.php?id=$1',
 		);
 
 		$config['category'] = array(
+			'alias'         => 'cookbook',
 			'regex'			=> '^recepten/categorie/(.*)$',
 			'sef'			=> 'recepten/categorie/{id}/{name}',
 			'redirect'		=> '{e_PLUGIN}cookbook/index.php?category=$1',
 		);
 
 		$config['categories'] = array(
+			'alias'         => 'cookbook',
 			'regex'			=> '^recepten/categorieen(.*)$',
 			'sef'			=> 'recepten/categorieen/',
 			'redirect'		=> '{e_PLUGIN}cookbook/index.php?category=0',
 		);
 
 		$config['tag'] = array(
+			'alias'         => 'cookbook',
 			'regex'			=> '^recepten/tag/(.*)$',
 			'sef'			=> 'recepten/tag/{tag}',
 			'redirect'		=> '{e_PLUGIN}cookbook/index.php?tag=$1',
 		);
 
 		$config['tags'] = array(
+			'alias'         => 'cookbook',
 			'regex'			=> '^recepten/tags(.*)$',
 			'sef'			=> 'recepten/tags/',
 			'redirect'		=> '{e_PLUGIN}cookbook/index.php?tag=0',
 		);
 
 		$config['recent'] = array(
+			'alias'         => 'cookbook',
 			'regex'			=> '^recepten/recent(.*)$',
 			'sef'			=> 'recepten/recent/',
 			'redirect'		=> '{e_PLUGIN}cookbook/recent.php',
