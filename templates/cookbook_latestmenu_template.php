d32a2c65b867a593a9590962f520a9ce6e541b16
Moc <github@tijnkuyper.nl>
2022-09-25 12:52:49 +0200

    Add optional template to latest recipe menu
    Co-Authored-By: Jimako <5429548+Jimmi08@users.noreply.github.com>

--- templates/cookbook_latestmenu_template.php
+++ templates/cookbook_latestmenu_template.php
@@ -14,5 +14,36 @@ $COOKBOOK_LATESTMENU_TEMPLATE = array();
           
 $COOKBOOK_LATESTMENU_TEMPLATE['default']['caption'] = '{COOKBOOK_LATESTMENU_CAPTION}';
 $COOKBOOK_LATESTMENU_TEMPLATE['default']['start']   = '<div class="cookbook_latest_menu">';
-$COOKBOOK_LATESTMENU_TEMPLATE['default']['item']    = "{COOKBOOK_RECIPE_NAME=no_url} <br>";
+$COOKBOOK_LATESTMENU_TEMPLATE['default']['item']    = "
+<div class='row'>
+    <div class='col-md-4 my-2'>
+        {SETIMAGE: w=200}{COOKBOOK_RECIPE_THUMB}
+    </div>
+
+    <div class='col-md-8 my-2'>
+        {COOKBOOK_RECIPE_NAME}
+    </div>
+</div>
+";
 $COOKBOOK_LATESTMENU_TEMPLATE['default']['end']     = '</div>';
+
+
+
+$COOKBOOK_LATESTMENU_TEMPLATE['image-title']['caption'] = '{COOKBOOK_LATESTMENU_CAPTION}';
+$COOKBOOK_LATESTMENU_TEMPLATE['image-title']['start']   = '
+<div class="cookbook_latest_menu m-4">';
+
+$COOKBOOK_LATESTMENU_TEMPLATE['image-title']['item']    = '
+ <div class="cookbook_latest_menu-item d-flex align-items-center my-2">
+  <div class="cookbook_latest_menu-item-image w-50">
+    {SETIMAGE: w=100}{COOKBOOK_RECIPE_THUMB}
+  </div>
+  <div class="cookbook_latest_menu-item-name flex-grow-2 ms-3">
+    {COOKBOOK_RECIPE_NAME}
+  </div>
+</div>';
+
+$COOKBOOK_LATESTMENU_TEMPLATE['image-title']['end']     = ' </div>';
+
+
+//$COOKBOOK_LATESTMENU_TEMPLATE['default'] = $COOKBOOK_LATESTMENU_TEMPLATE['image-title'];
